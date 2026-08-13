96c2a1420f296a330a723822fb0330e6618495dc
Javad Ghased <ghased.omp@gmail.com>
2026-08-13 22:01:27 +0330

    fix

--- apps/angular-app/src/app/app.component.ts
+++ apps/angular-app/src/app/app.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  encapsulation: ViewEncapsulation.None,
+  styleUrls: ['./app.component.css', '../styles.css']
 })
 export class AppComponent {
   title = 'angular-app';
